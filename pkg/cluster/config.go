ec98447a289ba825bc356380e2f1a9509ea95052
pandorasNox <itspandorasnox@gmail.com>
2021-02-05 19:34:22 +0100

    update cluster config

--- pkg/cluster/config.go
+++ pkg/cluster/config.go
@@ -2,8 +2,9 @@ package cluster
 
 // Config for the cluster
 type Config struct {
-	Version string  `yaml:"version"`
-	Cluster Cluster `yaml:"cluster"`
+	Version     string   `yaml:"version"`
+	Cluster     Cluster  `yaml:"cluster"`
+	ToolsServer struct{} `yaml:"toolsServer"`
 }
 
 // Cluster related information
@@ -13,8 +14,15 @@ type Cluster struct {
 }
 
 type servers struct {
-	Master masterServers `yaml:"master"`
-	Worker workerServers `yaml:"worker"`
+	SSHAuthorizedKeys []struct {
+		Name      string `yaml:"name"`
+		PublicKey string `yaml:"public_key"`
+	} `yaml:"ssh_authorized_keys"`
+	Static []struct{} `yaml:"static"`
+	Groups struct {
+		Master masterServers `yaml:"master"`
+		Worker workerServers `yaml:"worker"`
+	} `yaml:"groups"`
 }
 
 type masterServers []server
@@ -22,5 +30,21 @@ type masterServers []server
 type workerServers []server
 
 type server struct {
-	Name string `yaml:"name"`
+	BaseName                        string                  `yaml:"baseName"`
+	Scale                           int                     `yaml:"scale"`
+	EnablePossibilityToProxyKubectl bool                    `yaml:"enablePossibilityToProxyKubectl"`
+	ProviderMachineTemplate         providerMachineTemplate `yaml:"providerMachineTemplate"`
+}
+
+type providerMachineTemplate struct {
+	HetznerCloud hetznerCloudMachineProvider `yaml:"hetznerCloud"`
+}
+
+type hetznerCloudMachineProvider struct {
+	ServerType string `yaml:"serverType"`
+	Image      struct {
+		Name string `yaml:"name"`
+	} `yaml:"image"`
+	Location string            `yaml:"location"`
+	Labels   map[string]string `yaml:"labels"`
 }
