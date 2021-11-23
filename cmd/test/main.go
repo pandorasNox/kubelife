96e12e1d431e3d843ff5c883ec5503e141779625
pandorasNox <itspandorasnox@gmail.com>
2021-11-23 22:52:53 +0100

    add pointer test yaml parse

---
+++ cmd/test/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+
+	goyaml "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Point *Point `yaml:"point"`
+}
+
+type Point struct {
+	X int `yaml:"x"`
+	Y int `yaml:"y"`
+}
+
+func main() {
+	sampleYAML := `
+---
+point:
+  x: 5
+  y: 6
+`
+
+	sampleYAMLBytes := []byte(sampleYAML)
+	dec := goyaml.NewDecoder(bytes.NewReader(sampleYAMLBytes))
+
+	var config Config
+	err := dec.Decode(&config)
+	if err == io.EOF {
+		return
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(config.Point.X)
+
+	// configBytes, err := goyaml.Marshal(config)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(string(configBytes))
+}
